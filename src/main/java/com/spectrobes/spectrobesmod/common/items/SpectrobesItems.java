ecdd1ebc6ff270a089738d6eaff6f4d18e89e266
Aidan <aidanpauldavis@yahoo.co.uk>
2020-07-19 01:38:25 +0100

    finished the attack and defence mineral collections

--- src/main/java/com/spectrobes/spectrobesmod/common/items/SpectrobesItems.java
+++ src/main/java/com/spectrobes/spectrobesmod/common/items/SpectrobesItems.java
@@ -3,6 +3,7 @@ package com.spectrobes.spectrobesmod.common.items;
 import com.spectrobes.spectrobesmod.SpectrobesInfo;
 import com.spectrobes.spectrobesmod.SpectrobesMod;
 import com.spectrobes.spectrobesmod.common.items.fossils.KomainuFossilItem;
+import com.spectrobes.spectrobesmod.common.items.minerals.Mineral;
 import com.spectrobes.spectrobesmod.common.items.minerals.MineralItem;
 import com.spectrobes.spectrobesmod.common.items.minerals.MineralProperties;
 import com.spectrobes.spectrobesmod.common.registry.MineralRegistry;
@@ -24,45 +25,39 @@ public class SpectrobesItems {
     public static final Item mineral_item_power_c = null;
     public static final Item mineral_item_power_b = null;
     public static final Item mineral_item_power_a = null;
-    public static final Item komainu_fossil_item = null;
-    public static final Item[] ALL_MINERALS = new Item[] {
-            mineral_item_power_c,
-            mineral_item_power_b,
-            mineral_item_power_a
-    };
+    public static final Item mineral_item_power_a_plus = null;
+    public static final Item mineral_item_defence_c = null;
+    public static final Item mineral_item_defence_b = null;
+    public static final Item mineral_item_defence_a = null;
+    public static final Item mineral_item_defence_a_plus = null;
 
-    static void registerMineral(final RegistryEvent.Register<Item> event) {
-        event.getRegistry().register(
-                new MineralItem(
-                        new Item.Properties()
-                                .group(SpectrobesMineralItemGroup.Instance),
-                        MineralRegistry.MINERAL_POWER_C.name,
-                        MineralRegistry.MINERAL_POWER_C.properties.copy()));
-    }
+    public static final Item komainu_fossil_item = null;
+
 
     @SubscribeEvent
     public static void registerItems(final RegistryEvent.Register<Item> event) {
         MineralRegistry.init();
 
-        registerMineral(event);
-        event.getRegistry().register(
-                new MineralItem(
-                        new Item.Properties()
-                                .group(SpectrobesMineralItemGroup.Instance),
-                        MineralRegistry.MINERAL_POWER_B.name,
-                        MineralRegistry.MINERAL_POWER_B.properties.copy()));
-        event.getRegistry().register(
-                new MineralItem(
-                        new Item.Properties()
-                                .group(SpectrobesMineralItemGroup.Instance),
-                        MineralRegistry.MINERAL_POWER_A.name,
-                        MineralRegistry.MINERAL_POWER_A.properties.copy()));
+        for(Mineral m : MineralRegistry.ALL_MINERALS) {
+            registerMineral(event, m);
+        }
 
         event.getRegistry().register(
                 new KomainuFossilItem(
                         new Item.Properties()
                                 .group(SpectrobesItemGroup.Instance)));
     }
+
+    static void registerMineral(final RegistryEvent.Register<Item> event, Mineral mineral) {
+        event.getRegistry().register(
+                new MineralItem(
+                        new Item.Properties()
+                                .group(SpectrobesMineralItemGroup.Instance),
+                        mineral.name,
+                        mineral.properties.copy()));
+    }
+
+    //Creative Tabs
 
     public static class SpectrobesItemGroup extends ItemGroup {
 
@@ -91,4 +86,5 @@ public class SpectrobesItems {
             return SpectrobesItems.mineral_item_power_c.getDefaultInstance();
         }
     }
+
 }
--- src/main/java/com/spectrobes/spectrobesmod/common/registry/MineralRegistry.java
+++ src/main/java/com/spectrobes/spectrobesmod/common/registry/MineralRegistry.java
@@ -5,10 +5,19 @@ import com.spectrobes.spectrobesmod.common.spectrobes.SpectrobeProperties;
 import com.spectrobes.spectrobesmod.util.MineralBuilder;
 import com.spectrobes.spectrobesmod.util.MineralPropertiesBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MineralRegistry {
     public static Mineral MINERAL_POWER_C = null;
     public static Mineral MINERAL_POWER_B = null;
     public static Mineral MINERAL_POWER_A = null;
+    public static Mineral MINERAL_POWER_A_PLUS = null;
+    public static Mineral MINERAL_DEFENCE_C = null;
+    public static Mineral MINERAL_DEFENCE_B = null;
+    public static Mineral MINERAL_DEFENCE_A = null;
+    public static Mineral MINERAL_DEFENCE_A_PLUS = null;
+    public static List<Mineral> ALL_MINERALS = new ArrayList<>();
 
     public static void init()
     {
@@ -17,15 +26,48 @@ public class MineralRegistry {
                 .setAtkOffset(8)
                 .setNature(SpectrobeProperties.Nature.OTHER)
                 .setXpWorth(10)).build());
+        ALL_MINERALS.add(MINERAL_POWER_C);
         MINERAL_POWER_B = (new MineralBuilder().withName("mineral_item_power_b").withMineralProperties(
                 new MineralPropertiesBuilder()
                         .setAtkOffset(16)
                         .setNature(SpectrobeProperties.Nature.OTHER)
                         .setXpWorth(15)).build());
+        ALL_MINERALS.add(MINERAL_POWER_B);
         MINERAL_POWER_A = (new MineralBuilder().withName("mineral_item_power_a").withMineralProperties(
                 new MineralPropertiesBuilder()
                         .setAtkOffset(24)
                         .setNature(SpectrobeProperties.Nature.OTHER)
                         .setXpWorth(20)).build());
+        ALL_MINERALS.add(MINERAL_POWER_A);
+        MINERAL_POWER_A_PLUS = (new MineralBuilder().withName("mineral_item_power_a_plus").withMineralProperties(
+                new MineralPropertiesBuilder()
+                        .setAtkOffset(32)
+                        .setNature(SpectrobeProperties.Nature.OTHER)
+                        .setXpWorth(30)).build());
+        ALL_MINERALS.add(MINERAL_POWER_A_PLUS);
+        MINERAL_DEFENCE_C = (new MineralBuilder().withName("mineral_item_defence_c").withMineralProperties(
+                new MineralPropertiesBuilder()
+                        .setDefOffset(8)
+                        .setNature(SpectrobeProperties.Nature.OTHER)
+                        .setXpWorth(10)).build());
+        ALL_MINERALS.add(MINERAL_DEFENCE_C);
+        MINERAL_DEFENCE_B = (new MineralBuilder().withName("mineral_item_defence_b").withMineralProperties(
+                new MineralPropertiesBuilder()
+                        .setDefOffset(16)
+                        .setNature(SpectrobeProperties.Nature.OTHER)
+                        .setXpWorth(15)).build());
+        ALL_MINERALS.add(MINERAL_DEFENCE_B);
+        MINERAL_DEFENCE_A = (new MineralBuilder().withName("mineral_item_defence_a").withMineralProperties(
+                new MineralPropertiesBuilder()
+                        .setDefOffset(24)
+                        .setNature(SpectrobeProperties.Nature.OTHER)
+                        .setXpWorth(20)).build());
+        ALL_MINERALS.add(MINERAL_DEFENCE_A);
+        MINERAL_DEFENCE_A_PLUS = (new MineralBuilder().withName("mineral_item_defence_a_plus").withMineralProperties(
+                new MineralPropertiesBuilder()
+                        .setDefOffset(32)
+                        .setNature(SpectrobeProperties.Nature.OTHER)
+                        .setXpWorth(30)).build());
+        ALL_MINERALS.add(MINERAL_DEFENCE_A_PLUS);
     }
 }
