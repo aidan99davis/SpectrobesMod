d955db5a85e33bffd9bcfadce9483825d550ff46
Aidan <aidanpauldavis1999@gmail.com>
2022-05-27 16:15:49 +0100

    suppressed warnings

--- src/main/java/com/spectrobes/spectrobesmod/common/blocks/FossilBlock.java
+++ src/main/java/com/spectrobes/spectrobesmod/common/blocks/FossilBlock.java
@@ -6,26 +6,20 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
-import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.LootContext;
 
 import net.minecraft.loot.LootParameters;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.server.ServerWorld;
-import net.minecraftforge.common.BiomeDictionary;
-import net.minecraftforge.common.BiomeManager;
 import net.minecraftforge.common.ToolType;
 
-import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FossilBlock extends SpectrobesBlock {
-    private static Properties props = Properties.of(Material.STONE)
+    private static final Properties props = Properties.of(Material.STONE)
             .harvestTool(ToolType.PICKAXE)
             .strength(1.5f)
             .sound(SoundType.STONE)
@@ -35,9 +29,11 @@ public class FossilBlock extends SpectrobesBlock {
         super(props);
     }
 
+    @SuppressWarnings({"deprecation", "NullableProblems"})
     @Override
     public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
         Vector3d vec = builder.getParameter(LootParameters.ORIGIN);
+        assert Minecraft.getInstance().level != null;
         Biome biome = Minecraft.getInstance().level.getBiome(new BlockPos(vec.x, vec.y, vec.z));
 
         ItemStack fossilItem;
@@ -67,7 +63,7 @@ public class FossilBlock extends SpectrobesBlock {
                 break;
         }
 
-        ArrayList fossil = new ArrayList();
+        ArrayList<ItemStack> fossil = new ArrayList<>();
         fossil.add(fossilItem);
 
         return fossil;
