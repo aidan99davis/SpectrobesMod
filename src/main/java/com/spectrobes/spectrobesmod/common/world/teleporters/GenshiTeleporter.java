443c1192d8b995cb1551899e8868a3323aed3655
Aidan <aidanpauldavis1999@gmail.com>
2022-10-22 20:45:13 +0100

    fixed player spawn placement on teleporting to and from genshi

--- src/main/java/com/spectrobes/spectrobesmod/common/world/teleporters/GenshiTeleporter.java
+++ src/main/java/com/spectrobes/spectrobesmod/common/world/teleporters/GenshiTeleporter.java
@@ -3,6 +3,7 @@ package com.spectrobes.spectrobesmod.common.world.teleporters;
 import com.spectrobes.spectrobesmod.common.blocks.FossilBlock;
 import com.spectrobes.spectrobesmod.common.blocks.PlanetaryTeleporterBlock;
 import com.spectrobes.spectrobesmod.common.registry.SpectrobesBlocks;
+import net.minecraft.block.AirBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.fluid.Fluids;
@@ -29,24 +30,22 @@ public class GenshiTeleporter implements ITeleporter {
             y = thisPos.getY();
         }
         BlockPos destPos = new BlockPos(thisPos.getX(), y, thisPos.getZ());
-        int tries = 0;
         while ((destWorld.getBlockState(destPos).getMaterial() != Material.AIR && !destWorld.getBlockState(destPos).canBeReplaced(Fluids.WATER)) &&
-                (destWorld.getBlockState(destPos.above()).getMaterial() != Material.AIR && !destWorld.getBlockState(destPos.above()).canBeReplaced(Fluids.WATER)) &&
-                tries < 25) {
+                (destWorld.getBlockState(destPos.above()).getMaterial() != Material.AIR && !destWorld.getBlockState(destPos.above()).canBeReplaced(Fluids.WATER))) {
             destPos = destPos.above(2);
-            tries++;
-        }
-        entity.setPos(destPos.getX(), destPos.getY(), destPos.getZ());
-        if (thisIsToDaylightDim) {
-            for (BlockPos checkPos : BlockPos.betweenClosed(destPos.below(10).west(10), destPos.above(10).east(10))) {
-                if (destWorld.getBlockState(checkPos).getBlock() instanceof FossilBlock) {
-                    break;
-                }
-            }
         }
         if(destWorld.getBlockStates(entity.getBoundingBox().inflate(10, 10, 10)).noneMatch(blockState -> blockState.getBlock() instanceof PlanetaryTeleporterBlock)) {
-            destWorld.setBlock(destPos, SpectrobesBlocks.planetary_teleporter.get().defaultBlockState(), 1);
+            destWorld.setBlock(destPos.above(3), SpectrobesBlocks.planetary_teleporter.get().defaultBlockState(), 1);
         }
+        for (BlockPos checkPos : BlockPos.betweenClosed(destPos.below(10).west(10), destPos.above(10).east(10))) {
+            if (destWorld.getBlockState(checkPos).getBlock() instanceof AirBlock
+                && destWorld.getBlockState(checkPos.above(1)).getBlock() instanceof AirBlock) {
+                entity.setPos(destPos.getX(), destPos.above(4).getY(), destPos.getZ());
+            }
+        }
+
+
+
         return entity;
     }
 }
